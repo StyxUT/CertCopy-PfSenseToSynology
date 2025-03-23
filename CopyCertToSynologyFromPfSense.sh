fb34da9c97f083778bee7e6836ebbfc2d774f52b
styxut <styxut@gmail.com>
2025-03-22 19:36:38 -0600

    include wildcard in example: to pick up all cert file types

--- CopyCertToSynologyFromPfSense.sh
+++ CopyCertToSynologyFromPfSense.sh
@@ -1,5 +1,5 @@
 # manually replace sections below with the name of the cert, and the scp port number
 mkdir -p /volume1/LetsEncrypt/<name of cert>
-scp -P<port number> -v pfsense@pfsense.home:/conf/acme/<name of cert>* /volume1/LetsEncrypt/<name of cert>
+scp -P<port number> -v pfsense@pfsense.home:/conf/acme/<name of cert>.* /volume1/LetsEncrypt/<name of cert>
 # remove test cert if present
 rm -f /volume1/LetsEncrypt/<name of cert>/*TEST*
